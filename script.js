829f376a892b5cb1c338a90eabd15d0e9da3a222
Anais Kabambi <anais.k@hotmail.be>
2021-10-11 13:54:01 +0200

    multipliers added

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const display_score = document.getElementById('score_display');
 const btn_m = document.getElementById("purchase_multiplier");
 const display_purchase = document.getElementById("purchase_display");
 
+
+
+
 let score = parseInt(display_score.textContent);
 console.log(score);
 
@@ -31,3 +34,26 @@ const multiplier = () => {
 };
 
 btn_m.addEventListener("click", buy_cursor);
+/*
+const buy_grandma = () => {
+    if (score >= 100) {
+        score -= 100;
+        setInterval(multiplier, 100000)
+        display_purchase.textContent = 'grandma purchased';
+        btn_m.removeEventListener('click', buy_grandma);
+    } else {
+        display_purchase.textContent = 'bake more cookies first!'
+    }
+    btn_m.addEventListener("click", buy_grandma);
+
+    const buy_farm = () => {
+        if (score >= 1000) {
+            score -= 1000;
+            setInterval(multiplier, 10000)
+            display_purchase.textContent = 'farm purchased';
+            btn_m.removeEventListener('click', buy_farm);
+        } else {
+            display_purchase.textContent = 'bake more cookies first!'
+        }
+        btn_m.addEventListener("click", buy_farm);
+}*/
