b1d22a382e4c065c8f0bf2ae13751580effe87d7
Naama Kaplan <nkaplany@gmail.com>
2021-10-12 15:49:12 +0200

    added functional bonus

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ console.log(score);
 buttons[0].addEventListener("click", () => {
     score += click_worth;
     display[0].textContent = score.toString();
-    console.log(click_worth);
 })
 
 //indicators for cursors, farms and grandmas
@@ -60,7 +59,6 @@ const multiplier = () => {
 //one click is worth the specified number of cookies
 const booster = () => {
     click_worth++;
-    console.log(click_worth);
 }
 
 //each purchase of a booster increases the click worth by one
@@ -87,59 +85,45 @@ const buy_booster = () => {
         display[4].textContent = 'bake more cookies first!'
     }
 }
-//create timer
 
-
-/*var timer = 30;
-var interval = setInterval(function() {
-    counter--;
-    if (timer <= 0) {
-        clearInterval(interval);
-        return;
-    }else{
-        console.log(timer);
-    }*/
-
-// const countdown = () => {
-//     if (time_left < 0) {
-//         clearInterval(timer);
-//         buttons[5].textContent = "500 cookies for a bonus";
-//     } else {
-//         time_left < 2 ? buttons[5].textContent = `${time_left} second to go!`: btn_o.textContent = `${time_left} seconds to go!`;
-//     }
-// }
-//
-// const time_left = 30;
-// let timer = setInterval(countdown, 1000);
-
-
-//const bonus = while (timer > 0) =>{
-//    click_worth *= 2;
-//    }
-//if (timer <= 0) {
-//    console.log(click_worth);
-//    }
-//}, 1000);
-    //todo: temporary doubling, while timer runs
-
-
-
-
-//each purchase of a booster increases the click worth by one
-//the price of each consecutive booster is doubled
+//while the clock runs, each click is worth double
 const buy_bonus = () => {
     if (score >= buttons[5].getAttribute('data-min')) {
         score -= buttons[5].getAttribute('data-min');
         display[0].textContent = score.toString();
-        bonus();
 
-        //code
+        //display in info section while the clock is running
+        information[4].textContent = `200% bonus active!`
 
-        //display in info section
-        info_o.textContent = `200% bonus active!`
+        // variable for the timer is declared but it's value is null before clicking
+        let start_timer = null;
+
+        //create countdown
+        let clock = 5;
+        click_worth *=2;
+        console.log(click_worth);
+        const countdown = () => {
+            if (clock > 0) {
+                clock--;
+                clock < 2 ? buttons[5].textContent = `${clock} second to go!`: buttons[5].textContent = `${clock} seconds to go!`;
+                display[5].textContent = "Bonus active, hurry!";
+            } else {
+                buttons[5].textContent = "500 cookies for a bonus";
+                click_worth /=2;
+                console.log(click_worth);
+                //stop countdown
+                clearInterval(start_timer);
+                information[4].textContent = "No bonus is currently running.";
+                display[5].textContent = "You don't have a bonus running...";
+            }
+        };
+
+        //start countdown
+        start_timer = setInterval(countdown, 1000);
+
 
     } else {
-        display_bonus.textContent = 'bake more cookies first!'
+        display[5].textContent = 'bake more cookies first!'
     }
 }
 
